ce6d7fec57a766cc995475733261596c355db395
unknown <lucassdo2003@gmail.com>
2024-10-01 16:39:35 -0300

    first

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text("Skip"),
+                    Icon(Icons.skip_next),
+                  ],
+                )),
+            Image.asset("assets/images/HomeDog.png"),
+            Center(
+              child: Text("Ready to make a"),
+            ),
+            Center(
+              child: Text("new friend?"),
+            ),
+            Spacer(),
+            Center(
+              child: Text("Select your location and simply search"),
+            ),
+            Center(
+              child: Text("for pets near you"),
+            ),
+            Spacer(),
+            Center(
+              child: ElevatedButton(onPressed: () {},
+               child: Text("Get Started")),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
